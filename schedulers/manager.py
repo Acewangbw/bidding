50d1c38105cfc5a35a3b8d06f45319a8c48abefc
vFeng <z1914007838@outlook.com>
2019-01-26 12:39:36 +0800

    fix-修改定时任务启动方式

--- schedulers/manager.py
+++ schedulers/manager.py
@@ -1,12 +1,11 @@
 # -*- coding:utf8 -*-
-
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from apps.szgbidding.jobs.szzfcg import Szzfcg
 
 
 def start_schedulers():
-    sched = BlockingScheduler()
+    sched = BackgroundScheduler()
     # TODO : add your job here
     sched.add_job(Szzfcg.run, trigger='cron', second='*/10')
 
